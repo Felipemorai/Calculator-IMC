4195dbb3931c4e1269772146c91ca7631a9ef3fc
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-08-10 09:21:48 -0300

    Upgrade

--- imc.js
+++ imc.js
@@ -3,3 +3,9 @@ const height = document.getElementById('height');
 const weight = document.getElementById('weight');
 const calculate = document.getElementById('calculate');
 const result = document.getElementById('result');
+
+function imc () {
+    alert ('fuck my pussy, babe.');
+}
+
+calculate.addEventListener('click', imc);
