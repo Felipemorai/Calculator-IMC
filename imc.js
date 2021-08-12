3a362700e6f12e116cd9b357056042520aa1e8aa
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-08-12 15:09:50 -0300

    Upgrade

--- imc.js
+++ imc.js
@@ -5,7 +5,7 @@ const calculate = document.getElementById('calculate');
 function imc () {
     const name = document.getElementById('name').value;
     const height = document.getElementById('height').value;
-    const weight = document.getElementById('weight').valeu;
+    const weight = document.getElementById('weight').value;
     const resulted = document.getElementById('resulted');
 
     if (name !== '' && height !== '' && weight !== '') {
@@ -15,8 +15,23 @@ function imc () {
         let classification = "";
 
         if (valueIMC < 18.5){
-            classification = "you're underweight"
+            classification = " underweight 'skinny'."
+        }else
+        if (valueIMC < 25){
+            classification = " good weight 'normal'."
+        }else
+        if (valueIMC < 30){
+            classification = " slightly overweight 'kinda fat'"
+        }else
+        if (valueIMC < 35){
+            classification = " grade I obesity 'fat'"
+        }else
+        if (valueIMC < 40){
+            classification = " grade II obesity 'really fat'"
+        }else {
+            classification = " grade III obesity. Be Cautious!!"
         }
+        
 
         resulted.textContent = `${name} your IMC is ${valueIMC} and you are ${classification}`;
 
